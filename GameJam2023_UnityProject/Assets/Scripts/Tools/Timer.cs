140fa6915f2a7ec5e90445bce7af2a2a73293549
erizzoalbuquerque <erizzoalbuquerque@gmail.com>
2023-10-25 10:50:28 -0300

    Added alarm script to project

---
+++ GameJam2023_UnityProject/Assets/Scripts/Tools/Timer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float _duration = 0.5f;
+    [SerializeField] UnityEvent _timeOut;
+
+    float _timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timer > 0f)
+        {
+            _timer -= Time.deltaTime;
+            
+            if (_timer <= 0f)
+                _timeOut.Invoke();
+        }        
+    }
+
+    void OnEnable()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        _timer = _duration;
+    }
+}
