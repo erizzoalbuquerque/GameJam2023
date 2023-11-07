dabcb3c711457de6ce669f9f8bb1697d2efaea05
erizzoalbuquerque <erizzoalbuquerque@gmail.com>
2023-11-07 00:15:25 -0300

    Added cutscene to different phases

--- GameJam2023_UnityProject/Assets/GamePhase.cs
+++ GameJam2023_UnityProject/Assets/GamePhase.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class GamePhase : MonoBehaviour
 {
     [SerializeField] float _musicPitch = 1.0f;
+    [SerializeField] float _delay = 1.0f;
     [SerializeField] int _numberOfCustomersAtSameTime = 1;
     [SerializeField] AudioSource _audioSource;
-    [SerializeField] AudioClip _newPhaseSound;
 
     // Start is called before the first frame update
     void OnEnable()
     {
         GameManager.Instance.SetMusicPitch(_musicPitch);
         GameManager.Instance.SetNumberOfCustomersAtSameTime(_numberOfCustomersAtSameTime);
-        
-        if (_newPhaseSound != null)
-            _audioSource.PlayOneShot(_newPhaseSound);
+
+        if (_audioSource != null)
+            Invoke("PlaySound", _delay);
+    }
+
+    void PlaySound()
+    {
+        _audioSource.Play();
     }
 }
